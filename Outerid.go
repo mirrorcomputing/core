453132b441a8b410d5062f9d6e8adb7d819bbe60
timeloveboy <734991033@qq.com>
2016-09-20 14:25:01 +0800

    ~

---
+++ Outerid.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"time"
+)
+
+/*
+外世界ID
+
+30 byte:time
+20 byte:addr
+*/
+type Outerid struct {
+	time time.Time
+	addr string
+}
+
+func Unmarshal(bs []byte) (self *Outerid, err error) {
+	self.time, err = time.Parse("2009-11-10 23:00:00", string(bs[:30]))
+	self.addr = string(bs[30:])
+	return self, err
+}
+func (this *Outerid) Marshal() (bs []byte) {
+	ts := []byte(this.time.String()[:19])
+	ads := []byte(this.addr)
+	bs = make([]byte, 50)
+	copy(bs[:30], ts)
+	copy(bs[30:], ads)
+	return bs
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mirrorcore
+
+Outerid
+
+30 byte:time
+20 byte:addr
