597c753be5e67e7d87d346feb721cde3f3401b23
timeloveboy <734991033@qq.com>
2016-09-20 14:32:08 +0800

    ~

--- Outerid.go
+++ Outerid.go
@@ -4,6 +4,8 @@ import (
 	"time"
 )
 
+const OuteridLen = 50
+
 /*
 外世界ID
 
@@ -11,18 +13,18 @@ import (
 20 byte:addr
 */
 type Outerid struct {
-	time time.Time
-	addr string
+	Time time.Time
+	Addr string
 }
 
 func Unmarshal(bs []byte) (self *Outerid, err error) {
-	self.time, err = time.Parse("2009-11-10 23:00:00", string(bs[:30]))
-	self.addr = string(bs[30:])
+	self.Time, err = time.Parse("2009-11-10 23:00:00", string(bs[:30]))
+	self.Addr = string(bs[30:])
 	return self, err
 }
 func (this *Outerid) Marshal() (bs []byte) {
-	ts := []byte(this.time.String()[:19])
-	ads := []byte(this.addr)
+	ts := []byte(this.Time.String()[:19])
+	ads := []byte(this.Addr)
 	bs = make([]byte, 50)
 	copy(bs[:30], ts)
 	copy(bs[30:], ads)
